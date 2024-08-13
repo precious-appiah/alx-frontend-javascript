affd5d24450254ca3c9045972daf7cf99778df5b
Awura Precious <aaprecious99@gmail.com>
2024-08-13 10:52:10 +0000

    task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const getListStudents = () => {
+  return [
+    { id: 1, firstName: "Guillaume", location: "San Francisco" },
+    { id: 2, firstName: "James", location: "Columbia" },
+    { id: 5, firstName: "Serena", location: "San Francisco" },
+  ];
+};
