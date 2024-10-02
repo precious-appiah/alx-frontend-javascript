b91b95d6e9b1313d4ead789d26a4413e61f48d83
Awura Precious <aaprecious99@gmail.com>
2024-10-02 22:04:50 +0000

    task 1

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,14 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = true;
+
+      if (success) resolve("success");
+      else {
+        reject('failure')
+      }
+    }, 2000);
+  });
+}
+
+module.exports = getResponseFromAPI;
