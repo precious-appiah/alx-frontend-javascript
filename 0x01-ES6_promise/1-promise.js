84ed8817984b79b029e1832ee20fde70d91730a2
Awura Precious <aaprecious99@gmail.com>
2024-10-02 23:34:15 +0000

    error fixes

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,4 @@
-function getFullResponseFromAPI (success) {
+function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) resolve({ status: 200, body: 'Success' });
     reject(new Error('The fake API is not working currently'));
--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,17 +1,16 @@
 import { createUser, uploadPhoto } from './utils';
 
-export default async function asyncUploadUser () {
+export default async function asyncUploadUser() {
   const photoRes = await uploadPhoto();
   const userRes = await createUser();
   if (photoRes == null || userRes == null) {
     return {
       photo: null,
-      user: null
-    };
-  } else {
-    return {
-      photo: photoRes,
-      user: userRes
+      user: null,
     };
   }
+  return {
+    photo: photoRes,
+    user: userRes,
+  };
 }
