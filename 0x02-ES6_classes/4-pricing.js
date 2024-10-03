2168aabc3086175bc1824ee0b666085c8ca23bb6
Awura Precious <aaprecious99@gmail.com>
2024-10-03 22:41:14 +0000

    task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,24 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(curr) {
+    this._currency = curr;
+  }
+}
