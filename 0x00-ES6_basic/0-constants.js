625ac8071860bf85aa7acf109d094262bfbf338a
Awura Precious <aaprecious99@gmail.com>
2024-10-04 08:16:48 +0000

    init

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,9 @@
+export function taskFirst() {
+  const firstStr = "I prefer const when I can.";
+  return firstStr;
+}
+
+export function taskNext() {
+  let secStr = "But sometimes let is okay";
+  return secStr;
+}
